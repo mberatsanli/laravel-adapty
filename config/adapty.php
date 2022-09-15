de34261d25746da6d832c5ca6c54855b4be8aefb
melih <dev.mberatsanli@gmail.com>
2022-09-15 17:16:28 +0300

    events taken from config

--- config/adapty.php
+++ config/adapty.php
@@ -1,5 +1,23 @@
 <?php
 
+use MBS\LaravelAdapty\Events\AccessLevelUpdated;
+use MBS\LaravelAdapty\Events\BillingIssueDetected;
+use MBS\LaravelAdapty\Events\EnteredGracePeriod;
+use MBS\LaravelAdapty\Events\NonSubscriptionPurchase;
+use MBS\LaravelAdapty\Events\NonSubscriptionPurchaseRefunded;
+use MBS\LaravelAdapty\Events\SubscriptionExpired;
+use MBS\LaravelAdapty\Events\SubscriptionPaused;
+use MBS\LaravelAdapty\Events\SubscriptionRefunded;
+use MBS\LaravelAdapty\Events\SubscriptionRenewalCancelled;
+use MBS\LaravelAdapty\Events\SubscriptionRenewalReactivated;
+use MBS\LaravelAdapty\Events\SubscriptionRenewed;
+use MBS\LaravelAdapty\Events\SubscriptionStarted;
+use MBS\LaravelAdapty\Events\TrialConverted;
+use MBS\LaravelAdapty\Events\TrialExpired;
+use MBS\LaravelAdapty\Events\TrialRenewalCancelled;
+use MBS\LaravelAdapty\Events\TrialRenewalReactivated;
+use MBS\LaravelAdapty\Events\TrialStarted;
+
 return [
 
     'base_url' => env('ADAPTY_BASE_URL', 'https://api.adapty.io/api/v1/sdk'),
@@ -10,6 +28,29 @@ return [
         'path' => env('ADAPTY_WEBHOOK_PATH', '/adapty/webhook'),
         'middleware' => [],
         'reject_if_customer_user_id_is_null' => false,
+        'events' => [
+            // Subscriptions
+            'subscription_started' => SubscriptionStarted::class,
+            'subscription_renewed' => SubscriptionRenewed::class,
+            'subscription_renewal_cancelled' => SubscriptionRenewalCancelled::class,
+            'subscription_renewal_reactivated' => SubscriptionRenewalReactivated::class,
+            'subscription_expired' => SubscriptionExpired::class,
+            'subscription_paused' => SubscriptionPaused::class,
+            'non_subscription_purchase' =>  NonSubscriptionPurchase::class,
+            // Trials
+            'trial_started' => TrialStarted::class,
+            'trial_converted' => TrialConverted::class,
+            'trial_renewal_cancelled' => TrialRenewalCancelled::class,
+            'trial_renewal_reactivated' => TrialRenewalReactivated::class,
+            'trial_expired' => TrialExpired::class,
+            // Issues
+            'entered_grace_period' => EnteredGracePeriod::class,
+            'billing_issue_detected' => BillingIssueDetected::class,
+            'subscription_refunded' => SubscriptionRefunded::class,
+            'non_subscription_purchase_refunded' => NonSubscriptionPurchaseRefunded::class,
+            // System
+            'access_level_updated' => AccessLevelUpdated::class,
+        ],
     ],
 
 ];
--- src/Http/Controllers/AdaptyWebhookController.php
+++ src/Http/Controllers/AdaptyWebhookController.php
@@ -8,52 +8,11 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Log;
 use MBS\LaravelAdapty\Events\AbstractEvent;
-use MBS\LaravelAdapty\Events\AccessLevelUpdated;
-use MBS\LaravelAdapty\Events\BillingIssueDetected;
-use MBS\LaravelAdapty\Events\EnteredGracePeriod;
-use MBS\LaravelAdapty\Events\NonSubscriptionPurchase;
-use MBS\LaravelAdapty\Events\NonSubscriptionPurchaseRefunded;
-use MBS\LaravelAdapty\Events\SubscriptionExpired;
-use MBS\LaravelAdapty\Events\SubscriptionPaused;
-use MBS\LaravelAdapty\Events\SubscriptionRefunded;
-use MBS\LaravelAdapty\Events\SubscriptionRenewalCancelled;
-use MBS\LaravelAdapty\Events\SubscriptionRenewalReactivated;
-use MBS\LaravelAdapty\Events\SubscriptionRenewed;
-use MBS\LaravelAdapty\Events\SubscriptionStarted;
-use MBS\LaravelAdapty\Events\TrialConverted;
-use MBS\LaravelAdapty\Events\TrialExpired;
-use MBS\LaravelAdapty\Events\TrialRenewalCancelled;
-use MBS\LaravelAdapty\Events\TrialRenewalReactivated;
-use MBS\LaravelAdapty\Events\TrialStarted;
 
 class AdaptyWebhookController extends BaseController
 {
 
     use DispatchesJobs, ValidatesRequests;
-
-    protected $eventMap = [
-        // Subscriptions
-        'subscription_started' => SubscriptionStarted::class,
-        'subscription_renewed' => SubscriptionRenewed::class,
-        'subscription_renewal_cancelled' => SubscriptionRenewalCancelled::class,
-        'subscription_renewal_reactivated' => SubscriptionRenewalReactivated::class,
-        'subscription_expired' => SubscriptionExpired::class,
-        'subscription_paused' => SubscriptionPaused::class,
-        'non_subscription_purchase' =>  NonSubscriptionPurchase::class,
-        // Trials
-        'trial_started' => TrialStarted::class,
-        'trial_converted' => TrialConverted::class,
-        'trial_renewal_cancelled' => TrialRenewalCancelled::class,
-        'trial_renewal_reactivated' => TrialRenewalReactivated::class,
-        'trial_expired' => TrialExpired::class,
-        // Issues
-        'entered_grace_period' => EnteredGracePeriod::class,
-        'billing_issue_detected' => BillingIssueDetected::class,
-        'subscription_refunded' => SubscriptionRefunded::class,
-        'non_subscription_purchase_refunded' => NonSubscriptionPurchaseRefunded::class,
-        // System
-        'access_level_updated' => AccessLevelUpdated::class,
-    ];
 
     public function webhook(Request $request)
     {
@@ -72,14 +31,15 @@ class AdaptyWebhookController extends BaseController
                 throw new \Exception('Customer user id cannot be null!');
             }
 
+            $events = config('adapty.webhook.events');
             $eventType = $request->get('event_type');
 
-            if (!array_key_exists($eventType, $this->eventMap)) {
+            if (!array_key_exists($eventType, $events)) {
                 throw new \Exception('Undefined event! ::: ' . $eventType); // TODO change exception
             }
 
             /** @var AbstractEvent $event */
-            $event = $this->eventMap[$eventType];
+            $event = $events[$eventType];
             event($event::fromArray($request->all()));
 
             return response()->json([]);
